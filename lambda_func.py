7e8874da2d2b3d417c27a7cb6cececa18350f5a5
estebancabreraa <esteban.cabrera.a@gmail.com>
2022-11-16 00:38:10 -0600

    sum fixed, added mult, succ, power && menu is complete.

--- lambda_func.py
+++ lambda_func.py
@@ -1,26 +1,14 @@
 alpha = lambda x: x + 1
 beta = lambda x: 2 * x
 
-cero = lambda f, x: x
-uno = lambda f, x: f(x)
-dos = lambda f, x: f(f(x))
-tres = lambda f, x: f(f(f(x)))
+cero = lambda f: lambda a: a
+uno = lambda f: lambda a: f(a)
+dos = lambda f: lambda a: f(f(a))
+tres = lambda f: lambda a: f(f(f(a)))
+sucesor = lambda n: lambda f: lambda x: f(n(f)(x))
+suma = lambda a: lambda b: lambda f: lambda x: a(f)(b(f)(x))
+multiplicacion = lambda a: lambda b: lambda f: lambda x: a(b(f))(x)
+potencia = lambda a: lambda b: lambda f: lambda x: b(a)(f)(x)
 
-'''def cero(f, x):
-    return lambda f, x: x
 
-def uno(f, x):
-    return lambda f, x: f(x)
-
-def dos(f, x):
-    return lambda f, x: f(f(x))
-
-def tres(f, x):
-    return lambda f, x: f(f(f(x)))
-
-def suma(a, b, f, x):
-    return lambda a, b, f, x: a(f(b(f(x))))
-
-def sucesor(n):
-    return lambda a: a + n'''
 
--- main.py
+++ main.py
@@ -16,12 +16,24 @@ Menu:
     2. Uno(f, x).
     3. Dos(f, x).
     4. Tres(f, x).
+    5. Sucesor(n, f, x).
+    6. Suma(a, b, f, x).
+    7. Multiplacion(a, b, f, x).
+    8. Potencia(a, b, f, x).
+    9. Salir.
 '''
 
 functions = '''
 Funciones:
     1. Alpha.
     2. Beta.
+'''
+
+numbers = '''
+    1. Cero.
+    2. Uno.
+    3. Dos.
+    4. Tres.
 '''
 
 print(header)
@@ -41,7 +53,7 @@ while not exit:
             elif second_option == '2':
                 res = cero(beta, x)
                 print('\nResultado: ' + str(res))
-        if option == '2':
+        elif option == '2':
             print('Uno(f, x)')
             print(functions)
             second_option = input('Escoja la funcion f: ')
@@ -52,7 +64,7 @@ while not exit:
             elif second_option == '2':
                 res = uno(beta, x)
                 print('\nResultado: ' + str(res))
-        if option == '3':
+        elif option == '3':
             print('Dos(f, x)')
             print(functions)
             second_option = input('Escoja la funcion f: ')
@@ -63,7 +75,7 @@ while not exit:
             elif second_option == '2':
                 res = dos(beta, x)
                 print('\nResultado: ' + str(res))
-        if option == '4':
+        elif option == '4':
             print('Tres(f, x)')
             print(functions)
             second_option = input('Escoja la funcion f: ')
@@ -74,6 +86,124 @@ while not exit:
             elif second_option == '2':
                 res = tres(beta, x)
                 print('\nResultado: ' + str(res))
+        elif option == '5':
+            print('Sucesor(n, f, x)')
+            print(numbers)
+            second_option = input('Escoja n: ')
+            if second_option == '1':
+                numb1 = cero
+            elif second_option == '2':
+                numb1 = uno
+            elif second_option == '3':
+                numb1 = dos
+            elif second_option == '4':
+                numb1 = tres
+            print(functions)
+            third_option = input('Escoja la funcion f: ')
+            x = int(input('Ingrese el valor de x: '))
+            if third_option == '1':
+                res = sucesor(numb1)(alpha)(x)
+                print('\nResultado: ' + str(res))
+            elif third_option == '2':
+                res = sucesor(numb1)(beta)(x)
+                print('\nResultado: ' + str(res))
+        elif option == '6':
+            print('Suma(a, b, f, x)')
+            print(numbers)
+            second_option = input('Escoja a: ')
+            if second_option == '1':
+                numb1 = cero
+            elif second_option == '2':
+                numb1 = uno
+            elif second_option == '3':
+                numb1 = dos
+            elif second_option == '4':
+                numb1 = tres
+            print(numbers)
+            second_option = input('Escoja b: ')
+            if second_option == '1':
+                numb2 = cero
+            elif second_option == '2':
+                numb2 = uno
+            elif second_option == '3':
+                numb2 = dos
+            elif second_option == '4':
+                numb2 = tres
+            print(functions)
+            third_option = input('Escoja la funcion f: ')
+            x = int(input('Ingrese el valor de x: '))
+            if third_option == '1':
+                res = suma(numb1)(numb2)(alpha)(x)
+                print('\nResultado: ' + str(res))
+            elif third_option == '2':
+                res = suma(numb1)(numb2)(beta)(x)
+                print('\nResultado: ' + str(res))
+        elif option == '7':
+            print('Multiplicacion(a, b, f, x)')
+            print(numbers)
+            second_option = input('Escoja a: ')
+            if second_option == '1':
+                numb1 = cero
+            elif second_option == '2':
+                numb1 = uno
+            elif second_option == '3':
+                numb1 = dos
+            elif second_option == '4':
+                numb1 = tres
+            print(numbers)
+            second_option = input('Escoja b: ')
+            if second_option == '1':
+                numb2 = cero
+            elif second_option == '2':
+                numb2 = uno
+            elif second_option == '3':
+                numb2 = dos
+            elif second_option == '4':
+                numb2 = tres
+            print(functions)
+            third_option = input('Escoja la funcion f: ')
+            x = int(input('Ingrese el valor de x: '))
+            if third_option == '1':
+                res = multiplicacion(numb1)(numb2)(alpha)(x)
+                print('\nResultado: ' + str(res))
+            elif third_option == '2':
+                res = multiplicacion(numb1)(numb2)(beta)(x)
+                print('\nResultado: ' + str(res))
+        elif option == '8':
+            print('Potencia(a, b, f, x)')
+            print(numbers)
+            second_option = input('Escoja a: ')
+            if second_option == '1':
+                numb1 = cero
+            elif second_option == '2':
+                numb1 = uno
+            elif second_option == '3':
+                numb1 = dos
+            elif second_option == '4':
+                numb1 = tres
+            print(numbers)
+            second_option = input('Escoja b: ')
+            if second_option == '1':
+                numb2 = cero
+            elif second_option == '2':
+                numb2 = uno
+            elif second_option == '3':
+                numb2 = dos
+            elif second_option == '4':
+                numb2 = tres
+            print(functions)
+            third_option = input('Escoja la funcion f: ')
+            x = int(input('Ingrese el valor de x: '))
+            if third_option == '1':
+                res = potencia(dos)(tres)(alpha)(x)
+                print('\nResultado: ' + str(res))
+            elif third_option == '2':
+                res = potencia(dos)(tres)(alpha)(x)
+                print('\nResultado: ' + str(res))
+        elif option == '9':
+            exit = True
+        else:
+            print('La opcion seleccionada no existe.')
     except:
         print('Revise los valores ingresados.')
 
